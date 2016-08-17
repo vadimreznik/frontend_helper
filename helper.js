9bbd0023d5e8c49e01896cc88cc299d6e671e095
Guiseppe Braun <Guiseppe Braun>
2016-08-17 22:48:16 +0300

    'update'

--- helper.js
+++ helper.js
@@ -24,9 +24,36 @@ var settings = {
 		js: '(function(){})();'
 	}
 };
+if(!!proccess.argv[2] && proccess.argv[2].toLowerCase() === 'init'){
+	copyFile('helper.js', '../..');
+} else {
+	fs.mkdir(settings.folder.css);
+	fs.mkdir(settings.folder.js);
+	fs.appendFile(settings.folder.css + '/' + settings.file.css, settings.content.css);
+	fs.appendFile(settings.folder.js + '/' + settings.file.js, settings.content.js);
+	fs.appendFile(settings.file.html, settings.content.html);
+}
 
-fs.mkdir(settings.folder.css);
-fs.mkdir(settings.folder.js);
-fs.appendFile(settings.folder.css + '/' + settings.file.css, settings.content.css);
-fs.appendFile(settings.folder.js + '/' + settings.file.js, settings.content.js);
-fs.appendFile(settings.file.html, settings.content.html);
+function copyFile(source, target, cb) {
+	var cbCalled = false;
+
+	var rd = fs.createReadStream(source);
+	rd.on("error", function(err) {
+		done(err);
+	});
+	var wr = fs.createWriteStream(target);
+	wr.on("error", function(err) {
+		done(err);
+	});
+	wr.on("close", function(ex) {
+		done();
+	});
+	rd.pipe(wr);
+
+	function done(err) {
+		if (!cbCalled) {
+			cb(err);
+			cbCalled = true;
+		}
+	}
+}
